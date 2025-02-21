27d1ae8623ea9c61cc8d6055597ad358d050840e
Yazan26 <yazandawud75@gmail.com>
2025-02-21 23:35:48 +0100

    commit 2

--- MijnWebApi.WebApi/Program.cs
+++ MijnWebApi.WebApi/Program.cs
@@ -11,6 +11,11 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+// SQLConnectionString
+var sqlConnectionString = builder.COnfiguration.GetValue<string>("SqlConnectionString");
+var sqlCOnnectionStringFOund = !string.IsNullOrWhiteSpace(sqlConnectionString);
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
