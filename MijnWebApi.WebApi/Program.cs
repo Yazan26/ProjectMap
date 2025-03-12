f892d6c6f7dd5ca1225b5a5dbc379b8808f449d7
Yazan26 <yazandawud75@gmail.com>
2025-03-12 23:18:02 +0100

    fixed password must haves

--- MijnWebApi.WebApi/Program.cs
+++ MijnWebApi.WebApi/Program.cs
@@ -40,11 +40,11 @@ builder.Services
     .AddIdentityApiEndpoints<IdentityUser>(options =>
     {
         options.User.RequireUniqueEmail = true;
-        options.Password.RequiredLength = 8;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 10;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireDigit = true;
     })
     .AddRoles<IdentityRole>()
     .AddDapperStores(options =>
